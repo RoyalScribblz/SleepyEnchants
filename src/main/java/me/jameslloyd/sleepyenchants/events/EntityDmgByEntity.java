682bc16fc7f3ce8820e0059071f7cc5334f01091
RoyalScribblz <clashmonster123@gmail.com>
2022-01-27 16:12:11 +0000

    Added Ice Aspect snowball particles

--- src/main/java/me/jameslloyd/sleepyenchants/events/EntityDmgByEntity.java
+++ src/main/java/me/jameslloyd/sleepyenchants/events/EntityDmgByEntity.java
@@ -2,6 +2,8 @@ package me.jameslloyd.sleepyenchants.events;
 
 import me.jameslloyd.sleepyenchants.enchants.CustomEnchants;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.SoundCategory;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
@@ -24,10 +26,12 @@ public class EntityDmgByEntity implements Listener {
 
     Player player;
     ItemStack itemInHand;
+    LivingEntity entity;
 
     @EventHandler
     public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent e){
         Entity damager = e.getDamager();
+        entity = (LivingEntity) e.getEntity();
         if (damager instanceof Player) {
             player = (Player) damager;
             // player who damaged the entity
@@ -46,7 +50,7 @@ public class EntityDmgByEntity implements Listener {
         double trigger_chance = (itemInHand.getEnchantmentLevel(CustomEnchants.DEVILSSCYTHE) +1)  * 2;
 
         if(trigger <= trigger_chance) {
-            ((LivingEntity) e.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.HARM,
+            entity.addPotionEffect(new PotionEffect(PotionEffectType.HARM,
                     1 * 20, 1, true, false));
             player.playSound(player.getLocation(), "minecraft:custom.devilsscythe", SoundCategory.MASTER, 100, 1);
             sendMsg(player, "&aUsing the Devil's Scythe enchant!");
@@ -56,9 +60,21 @@ public class EntityDmgByEntity implements Listener {
 
     public void iceAspect(EntityDamageByEntityEvent e){
         int duration = 4 * (itemInHand.getEnchantmentLevel(CustomEnchants.ICEASPECT) + 1);
-        ((LivingEntity) e.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.SLOW,
+        ((LivingEntity) e.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.SLOW,  // add slowness effect
                 duration * 20, 1, true, false));
         player.playSound(player.getLocation(), "minecraft:custom.iceaspect", SoundCategory.MASTER, 100, 1);
+
+        // spawn 20 ice particles in random places around the hit entity
+        Location entityLoc = entity.getLocation();
+        for (int i = 0; i < 20; i++) {
+            double xOffset = ThreadLocalRandom.current().nextDouble();
+            double yOffset = ThreadLocalRandom.current().nextDouble();
+            double zOffset = ThreadLocalRandom.current().nextDouble();
+            Location particleLoc = entityLoc.clone().add(xOffset-0.5, yOffset+0.8, zOffset-0.5);
+
+            entity.getWorld().spawnParticle(Particle.SNOWBALL, particleLoc, 1);
+        }
+
         sendMsg(player, "&aUsing the Ice Aspect enchant!");
     }
 }
