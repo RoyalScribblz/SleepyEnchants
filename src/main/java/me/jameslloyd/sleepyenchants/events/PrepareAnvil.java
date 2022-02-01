6cdd480aa0e88a7c9eb5200bf15f46198290a798
RoyalScribblz <clashmonster123@gmail.com>
2022-02-01 13:42:49 +0000

    anvil increases enchant level

--- src/main/java/me/jameslloyd/sleepyenchants/events/PrepareAnvil.java
+++ src/main/java/me/jameslloyd/sleepyenchants/events/PrepareAnvil.java
@@ -10,6 +10,9 @@ import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class PrepareAnvil implements Listener {
@@ -28,14 +31,33 @@ public class PrepareAnvil implements Listener {
 
         if (result == null) return;
 
-        boolean hasCustomEnchant = false;
+        Map<Enchantment, Integer> enchantsToApply = new HashMap<Enchantment, Integer>();
         Set<Enchantment> all = CustomEnchants.getAll();
         for (Enchantment enchant : all) {
-            if (item1.containsEnchantment(enchant)) {ApplyEnchant.applyEnchant(result, (EnchantmentWrapper) enchant, item1.getEnchantmentLevel(enchant)); hasCustomEnchant=true;}
-            if (item2.containsEnchantment(enchant)) {ApplyEnchant.applyEnchant(result, (EnchantmentWrapper) enchant, item2.getEnchantmentLevel(enchant)); hasCustomEnchant=true;}
+            if (item1.containsEnchantment(enchant)) enchantsToApply.put(enchant, item1.getEnchantmentLevel(enchant));
+            if (item2.containsEnchantment(enchant)) {
+                if (!enchantsToApply.containsKey(enchant)) { // if it doesn't already contain add like normal
+                    enchantsToApply.put(enchant, item1.getEnchantmentLevel(enchant));
+                } else {
+                    int level1 = enchantsToApply.get(enchant);
+                    int level2 = item2.getEnchantmentLevel(enchant);
+
+                    if (level2 > level1) {
+                        enchantsToApply.replace(enchant, level2);
+                    }
+
+                    if (level2 == level1) {
+                        enchantsToApply.replace(enchant, level2+1);
+                    }
+                }
+            }
         }
 
-        if (!hasCustomEnchant) return;
+        if (enchantsToApply.size() == 0) return;
+
+        for (Enchantment enchant : enchantsToApply.keySet()) {
+            ApplyEnchant.applyEnchant(result, (EnchantmentWrapper) enchant, enchantsToApply.get(enchant));
+        }
 
         e.setResult(result);
     }
